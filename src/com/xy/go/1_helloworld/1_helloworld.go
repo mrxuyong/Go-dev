5705d558b21a402cbd01cde77fb9b118f9f86b06
xuyong <xuyong@qianmi.com>
2017-01-03 19:52:42 +0800

    go语言，代码整理

---
+++ src/com/xy/go/1_helloworld/1_helloworld.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+/**
+ * main包，main方法；
+ */
+func main() {
+	fmt.Println("GO, Hello World!");
+}
---
+++ src/com/xy/go/2_variable/2.1_go_variable.go
@@ -0,0 +1,13 @@
+//package __variable
+package main
+
+var a int = 10
+var b = 20
+//c : = 30
+
+//变量
+func main() {
+	//println("a-->>" + a)
+	//println(a)
+	println(a, b)
+}
